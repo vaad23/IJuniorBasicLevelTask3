5151310670978193d1bb39fb27d3dfc96c388278
vaad23 <vaad23@mail.ru>
2020-04-26 10:54:21 +0300

    version alpha 01

---
+++ Assets/Scripts/GroundTracking.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GroundTracking : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement _player;
+    [SerializeField] private Vector3 _playerPosition;
+    [SerializeField] private Vector3 _pastPlayerPosition;
+    private bool _isGround;
+
+    public delegate void PlayerState(PlayerMovement.State state);
+    public event PlayerState Changed;
+
+    private void Start()
+    {
+        _isGround = true;
+        _playerPosition = _player.gameObject.transform.position;
+        _pastPlayerPosition = new Vector3(_playerPosition.x, _playerPosition.y, _playerPosition.z);
+        /*
+        _pastPlayerPosition.x = _playerPosition.x;
+        _pastPlayerPosition.y = _playerPosition.y;
+        _pastPlayerPosition.z = _playerPosition.z;*/
+    }
+
+    private void Update()
+    {
+        Debug.Log(_playerPosition.x - _pastPlayerPosition.x);
+        Debug.Log(_playerPosition.y - _pastPlayerPosition.y);
+        if (Vector3.Distance(_pastPlayerPosition, _playerPosition) > 0.01)
+        {
+            if (_isGround)
+            {
+                if (Mathf.Abs(_playerPosition.x - _pastPlayerPosition.x) > 0.01)
+                    Changed?.Invoke(PlayerMovement.State.Run);
+            }
+            else
+            {
+                if (_playerPosition.y > _pastPlayerPosition.y)
+                    Changed?.Invoke(PlayerMovement.State.JumpUp);
+                else
+                    Changed?.Invoke(PlayerMovement.State.JumpDown);
+            }
+        }
+        else
+        {
+            if (_isGround)
+                Changed?.Invoke(PlayerMovement.State.Idle);
+            else
+                Changed?.Invoke(PlayerMovement.State.JumpDown);
+        }
+
+
+        _playerPosition = _player.gameObject.transform.position;
+        //_pastPlayerPosition = new Vector3(_playerPosition.x, _playerPosition.y, _playerPosition.z);
+          if (Mathf.Abs(_playerPosition.x - _pastPlayerPosition.x) > 0.01)
+              _pastPlayerPosition.x = _playerPosition.x;
+           if (Mathf.Abs(_playerPosition.y - _pastPlayerPosition.y) > 0.01)
+             _pastPlayerPosition.y = _playerPosition.y;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private GroundTracking groundTracking;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+    private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _sprite;
+    public State CurrentState { get; private set; }
+
+    public enum State
+    {
+        Idle,
+        Run,
+        JumpUp,
+        JumpDown
+    }
+
+    private void OnEnable()
+    {
+        groundTracking.Changed += StateChange;
+    }
+
+    private void OnDisable()
+    {
+        groundTracking.Changed -= StateChange;
+    }
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _sprite = GetComponent<SpriteRenderer>();
+        CurrentState = State.Idle;
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            Movement();
+        }
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            Run(false);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Run(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void Idle()
+    {
+
+    }
+
+    private void Run(bool flipX)
+    {
+        int direction = flipX ? -1 : 1;
+        if (_sprite.flipX != flipX)
+            _sprite.flipX = flipX;
+
+        transform.Translate(_speed * Time.deltaTime * direction, 0, 0);
+    }
+
+    private void StateChange(State state)
+    {
+        if (CurrentState != state)
+        {
+            CurrentState = state;
+            switch (CurrentState)
+            {
+                case State.Idle:
+                    Debug.Log("Idle");
+                    break;
+                case State.Run:
+                    Debug.Log("Run");
+                    break;
+                case State.JumpUp:
+                    Debug.Log("Up");
+                    break;
+                case State.JumpDown:
+                    Debug.Log("Down");
+                    break;
+            }
+        }
+    }
+}
