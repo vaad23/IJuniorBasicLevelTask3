deb5319701188491f3d4c74b9bd2d19c82245cb3
vaad23 <vaad23@mail.ru>
2020-04-29 08:54:43 +0300

    version alpha 05

--- Assets/Scripts/GroundTracking.cs
+++ Assets/Scripts/GroundTracking.cs
@@ -6,7 +6,6 @@ using UnityEngine.Events;
 public class GroundTracking : MonoBehaviour
 {
     private int _countGround;
-    // private bool _isGround;
 
     public bool IsGround { get; private set; }
     public int CountGround
@@ -32,25 +31,6 @@ public class GroundTracking : MonoBehaviour
         }
     }
 
-  /*  public bool IsGround
-    {
-        get
-        {
-            return _isGround;
-        }
-        private set
-        {
-            _isGround = value;
-            if (_isGround)
-                GroundFound?.Invoke(true);
-            else
-                GroundFound?.Invoke(false);
-        }
-    }
-
-    public event UnityAction<bool> GroundFound;
-
-*/
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out Ground ground))
